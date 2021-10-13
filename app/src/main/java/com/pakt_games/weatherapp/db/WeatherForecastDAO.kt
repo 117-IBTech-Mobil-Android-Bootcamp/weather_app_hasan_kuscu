49f836d6166529222e9c0b4bd016c78340d61689
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-13 23:27:33 +0300

    [DEV] created dao and database

--- app/src/main/java/com/pakt_games/weatherapp/db/WeatherForecastDAO.kt
+++ app/src/main/java/com/pakt_games/weatherapp/db/WeatherForecastDAO.kt
@@ -1,5 +1,14 @@
 package com.pakt_games.weatherapp.db
 
-interface WeatherForecastDAO {
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.pakt_games.weatherapp.ui.model.CityCurrent
 
+interface WeatherForecastDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie : CityCurrent)
+
+    @Query("SELECT * FROM CITYCURRENT")
+    suspend fun  fetchMovies() : List<CityCurrent>
 }
--- app/src/main/java/com/pakt_games/weatherapp/ui/model/CityCurrent.kt
+++ app/src/main/java/com/pakt_games/weatherapp/ui/model/CityCurrent.kt
@@ -1,9 +1,16 @@
 package com.pakt_games.weatherapp.ui.model
 
+import android.hardware.SensorManager
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "CITYCURRENT")
 data class CityCurrent(
-    val temp_c : Double,
-    val temp_f : Double,
-    val feelslike_c : Double,
-    val feelslike_f : Double,
-    val condution : CityCondution
+    @PrimaryKey @ColumnInfo(name = "id") val id : Int,
+    @ColumnInfo(name = "temp_c") val temp_c : Double,
+    @ColumnInfo(name = "temp_f") val temp_f : Double,
+    @ColumnInfo(name = "fellslike_c") val feelslike_c : Double,
+    @ColumnInfo(name = "fellslike_f") val feelslike_f : Double,
+    @ColumnInfo(name = "condution") val condution : CityCondution
 )
