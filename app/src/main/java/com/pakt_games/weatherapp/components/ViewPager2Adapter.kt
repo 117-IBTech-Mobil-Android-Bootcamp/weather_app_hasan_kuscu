621523c69f53e702c01f79dbc7bdbb1f6552c1ac
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-15 08:50:54 +0300

    [DEV] created WeatherForecastSelectedCityFragment

--- app/src/main/java/com/pakt_games/weatherapp/components/ViewPager2Adapter.kt
+++ app/src/main/java/com/pakt_games/weatherapp/components/ViewPager2Adapter.kt
@@ -4,9 +4,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.pakt_games.weatherapp.ui.view.WeatherForecastSearchFragment
+import com.pakt_games.weatherapp.ui.view.WeatherForecastSelectedCityFragment
 
 class ViewPager2Adapter(fragmentManager : FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int = 2
-    
+
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> return WeatherForecastSearchFragment()
+            1 -> return WeatherForecastSelectedCityFragment()
+            else -> return WeatherForecastSearchFragment()
+        }
+    }
 
 }
