2cb2675ae8c7efa6efacf0591312e50fb1b1631e
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-15 08:58:50 +0300

    [DEV] added databinding in WeatherForecastHomeFragment

--- app/src/main/java/com/pakt_games/weatherapp/ui/view/WeatherForecastHomeFragment.kt
+++ app/src/main/java/com/pakt_games/weatherapp/ui/view/WeatherForecastHomeFragment.kt
@@ -6,19 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pakt_games.weatherapp.R
+import com.pakt_games.weatherapp.base.BaseFragment
 
 
-class WeatherForecastHomeFragment : Fragment() {
+class WeatherForecastHomeFragment : BaseFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_forecast_home, container, false)
-    }
 }
