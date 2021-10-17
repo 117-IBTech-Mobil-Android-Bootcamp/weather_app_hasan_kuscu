1bf6ccae459969877f64a998e07afd69d30c832c
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-17 16:11:03 +0300

    [DEV] added new response field

--- app/src/main/java/com/pakt_games/weatherapp/network/response/WeatherForecastResponse.kt
+++ app/src/main/java/com/pakt_games/weatherapp/network/response/WeatherForecastResponse.kt
@@ -2,7 +2,9 @@ package com.pakt_games.weatherapp.network.response
 
 import com.google.gson.annotations.SerializedName
 import com.pakt_games.weatherapp.ui.model.CityCurrent
+import com.pakt_games.weatherapp.ui.model.CityLocation
 
 data class WeatherForecastResponse(
-    @SerializedName("current") val city : CityCurrent
+    @SerializedName("current") val city : CityCurrent,
+    @SerializedName("location") val cityName: CityLocation,
 )
