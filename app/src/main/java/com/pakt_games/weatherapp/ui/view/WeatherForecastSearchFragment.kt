9f6efb111061a72295d3667d7be2dd525bb5d8fa
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-20 10:23:35 +0300

    [DEV] designed and implemeing finished in Search Screen

--- app/src/main/java/com/pakt_games/weatherapp/ui/view/WeatherForecastSearchFragment.kt
+++ app/src/main/java/com/pakt_games/weatherapp/ui/view/WeatherForecastSearchFragment.kt
@@ -1,10 +1,10 @@
 package com.pakt_games.weatherapp.ui.view
 
+import android.view.View
 import android.widget.ArrayAdapter
 import com.pakt_games.weatherapp.R
 import com.pakt_games.weatherapp.base.BaseFragment
 import com.pakt_games.weatherapp.databinding.FragmentWeatherForecastSearchBinding
-import com.pakt_games.weatherapp.ui.model.CityLocation
 import com.pakt_games.weatherapp.ui.viewmodel.WeatherForecastSearchFragmentViewModel
 import com.pakt_games.weatherapp.utils.showToast
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -18,8 +18,9 @@ class WeatherForecastSearchFragment : BaseFragment<WeatherForecastSearchFragment
     override fun getLayoutID() = R.layout.fragment_weather_forecast_search
 
     override fun observeLiveData() {
-       dataBinding.imageViewSaveCity.setOnClickListener {
+       dataBinding.imageViewSearchButton.setOnClickListener {
            autoTextViewIsWork()
+           dataBinding.group.visibility = View.VISIBLE
        }
 
     }
@@ -29,9 +30,12 @@ class WeatherForecastSearchFragment : BaseFragment<WeatherForecastSearchFragment
         if(requestedCityName!="")
             viewModel.prepareCityName(requestedCityName!!)
         viewModel.onCityNameFetched.observe(this, {
-            var cityList= arrayListOf<Any>(it.getCityName(), "Adana")
+            var cityList= arrayListOf<Any>(it.getCityName())
             var adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,cityList)
             dataBinding.autoCompleteTextViewSearch.setAdapter(adapter)
+            dataBinding.model = it.getCity()
+            dataBinding.executePendingBindings()
+            dataBinding.textViewWeatherForecastSearchCityName.text=requestedCityName
 
         })
         viewModel.onError.observe(this, {
--- app/src/main/java/com/pakt_games/weatherapp/utils/DownloadImage.kt
+++ app/src/main/java/com/pakt_games/weatherapp/utils/DownloadImage.kt
@@ -7,11 +7,17 @@ import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.pakt_games.weatherapp.R
+import java.io.FileDescriptor.out
+import java.lang.System.out
 
 fun ImageView.downloadImage(url:String?, placeholder: CircularProgressDrawable) {
     //if  image to late coming this screen
-    val options= RequestOptions().placeholder(placeholder).error(R.mipmap.ic_launcher)
-    Glide.with(context).setDefaultRequestOptions(options).load(url!!.castedUrl()).into(this)
+    try {
+        val options= RequestOptions().placeholder(placeholder).error(R.mipmap.ic_launcher)
+        Glide.with(context).setDefaultRequestOptions(options).load(url!!.castedUrl()).into(this)
+    } catch (exeption:Exception) {
+        //Exeption
+    }
 }
 fun String.castedUrl(baseUrl:String?="https:"):String {
     return baseUrl+this
