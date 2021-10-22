0011bd6e77b96305a7e7991fe79fed8e447c025b
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-22 10:08:49 +0300

    [DEV] Implemented FragmentPagerAdapter members

--- app/src/main/java/com/pakt_games/weatherapp/components/CustomRegisteredCitiesPagerAdapter.kt
+++ app/src/main/java/com/pakt_games/weatherapp/components/CustomRegisteredCitiesPagerAdapter.kt
@@ -1,4 +1,19 @@
 package com.pakt_games.weatherapp.components
 
-class CustomRegisteredCitiesPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class CustomRegisteredCitiesPagerAdapter(manager: FragmentManager): FragmentPagerAdapter(manager) {
+
+    private val fragments = ArrayList<Fragment>()
+
+    override fun getItem(position: Int) : Fragment = fragments[position]
+
+    override fun getCount(): Int = fragments.size
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyDataSetChanged()
+    }
 }
