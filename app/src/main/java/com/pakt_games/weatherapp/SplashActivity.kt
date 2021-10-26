006b16dac948abccb2247caede344f2d29fe519f
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-26 21:26:08 +0300

    [DEV]  Splash Activity developed

--- app/src/main/java/com/pakt_games/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/pakt_games/weatherapp/SplashActivity.kt
@@ -2,10 +2,24 @@ package com.pakt_games.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import com.pakt_games.weatherapp.utils.startActivity
 
 class SplashActivity : AppCompatActivity() {
+    //Waiting 3 sec. for Splash Activity
+    private val DELAY: Long=3*1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Timer Thread
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity<HomeActivity>()
+                //destroyed splash activitiy
+                finish()
+            }
+        }, DELAY)
     }
 }
--- app/src/main/java/com/pakt_games/weatherapp/utils/extensions.kt
+++ app/src/main/java/com/pakt_games/weatherapp/utils/extensions.kt
@@ -1,7 +1,10 @@
 package com.pakt_games.weatherapp.utils
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 fun Fragment.showToast(messageToShow : String){
@@ -9,4 +12,12 @@ fun Fragment.showToast(messageToShow : String){
 }
 fun Activity.showToast(messageToShow : String){
     Toast.makeText(this, messageToShow, Toast.LENGTH_LONG).show()
+}
+inline fun<reified T : AppCompatActivity> Context.startActivity(block : Intent.() -> Unit = {}){
+    val intent  = Intent(this , T::class.java)
+    startActivity(
+        intent.also {
+            block.invoke(it)
+        }
+    )
 }
