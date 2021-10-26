7b50537aca4f13bdc1cf71d99c8c2399c8126810
kuscuhasann <kuscuhasann97@gmail.com>
2021-10-26 14:08:04 +0300

    [TEST] created FakeWeatherForecastSearchRepository

--- app/src/test/java/com/pakt_games/weatherapp/fakerepofortest/FakeWeatherForecastSearchRepository.kt
+++ app/src/test/java/com/pakt_games/weatherapp/fakerepofortest/FakeWeatherForecastSearchRepository.kt
@@ -1,4 +1,20 @@
 package com.pakt_games.weatherapp.fakerepofortest
 
-class FakeWeatherForecastSearchRepository {
+import com.pakt_games.weatherapp.network.response.WeatherForecastResponse
+import com.pakt_games.weatherapp.repository.RepositoryInterfaces.IWeatherForecastSearchRepository
+import com.pakt_games.weatherapp.ui.model.SavedCities
+import kotlinx.coroutines.flow.Flow
+
+class FakeWeatherForecastSearchRepository:IWeatherForecastSearchRepository {
+    override suspend fun getCityName(cityName: String): Flow<WeatherForecastResponse?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertCityAsync(savedCities: SavedCities) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTable() {
+        TODO("Not yet implemented")
+    }
 }
